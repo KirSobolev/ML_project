21dd236f8a25c22a12cbadb47053c36125da9a49
KirSobolev <kirill.sobolev51@gmail.com>
2024-12-04 17:08:05 +0200

    final update, creators name is added to filenames

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ INSTALL REQUIREMENTS FROM REQUIREMENTS.TXT
 It is recommended to create virtual enviroment (.venv).
 To install all python libraries at once use next command:
   pip install -r /path/to/requirements.txt
+
+
+Folders description:
+    web-site/ - everything related to website, done by Mykhailo
+    old_files/ - storage for files that are not used in the final solution
+        Vladimir/ - everything that Vladimir submitted
+        tests.ipynb - some data EDA, made by Kirill and Hezekiah
+        models_test_versions/ - stores models and working notebooks, made by Kirill and Hezekiah
+        marwis_mobile_join.ipynb - data prep for marwis dataset, were not used in the end
+    final/ - stores final solution, made by Kirill
+        model_train.ipynb - training notebook
+        demo.py - demo GUI
+        data_prep.ipynb - data prep for starwis and weather datasets
+        
+    ready_data/ - recommended path to use for storing processed data
+    original_data/  - recommended path to store original data
---
+++ final/demo_Kirill.py
@@ -0,0 +1,197 @@
+import pandas as pd
+import tkinter as tk
+import lime
+import lime.lime_tabular
+from tkcalendar import Calendar
+from xgboost import XGBRegressor
+from matplotlib.figure import Figure 
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+# Load dataset and model
+df = pd.read_csv("/Users/kirillsobolev/Documents/GitHub/ML_project/ready_data/winter_2023.csv")
+model = XGBRegressor()
+model.load_model("/Users/kirillsobolev/Documents/GitHub/ML_project/final/xgb_best.json")
+X_train = pd.read_csv("/Users/kirillsobolev/Documents/GitHub/ML_project/ready_data/winter_2021.csv")
+# Tinker variables 
+day_df = None
+predictions = None
+current_canvas = None
+slider = None
+
+# Columns for dataprep functions
+past4h_avg_columns = ['airTemp', 'humidity', 'dewpoint', 'precipitation', 'Friction',
+       'Road_Surface_Temperature', 'Water_Film_Surface_mm']
+
+forecast_columns = ['airTemp', 'humidity', 'dewpoint', 'precipitation']
+
+
+# Data prep functions
+def calculate_forecast(df, columns):
+    """
+    Imitates weather forecast using rolling average for next 2 hours 
+    """
+    for col in columns:
+        df[f"fcst_{col}"] = df[col][::-1].rolling(window=12, min_periods=1).mean()[::-1]
+    return df
+
+def calculate_avg_past(df, columns):
+    """
+    Calculates rolling average for 4 hours backwards. 
+    Helps the model to catch the trend better
+    """
+    for col in columns:
+        df[f"past4h_avg_{col}"] = df[col].rolling(window=24, min_periods=1).mean()
+    return df
+
+# preprocess the data
+df = calculate_forecast(df, forecast_columns)
+df = calculate_avg_past(df, past4h_avg_columns)
+df['ts'] = pd.to_datetime(df['ts'])
+X_train = calculate_avg_past(X_train, past4h_avg_columns)
+X_train = calculate_forecast(X_train, forecast_columns)
+X_train = X_train.drop("ts", axis=1)
+
+# Initialize LIME exaplainer to explain forecasts later
+explainer = lime.lime_tabular.LimeTabularExplainer(
+    training_data=X_train.values,
+    feature_names=X_train.columns,
+    mode='regression')
+
+# Create tkinter root window
+root = tk.Tk()
+ 
+# Set geometry
+root.geometry("800x800")
+
+# Add Calendar and a Frame to hold the calendar and button
+calendar_frame = tk.Frame(root)
+calendar_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nw")
+# Frame for data labels
+data_labels_frame = tk.Frame(root)
+data_labels_frame.grid(row=2, column=1, padx=10, pady=10, sticky="nw")
+# Calendar object
+cal = Calendar(calendar_frame, selectmode = 'day',
+               year = 2023, month = 12,
+               day = 22,
+               mindate=df.ts.min(),
+               maxdate=df.ts.max(),
+               foreground="black")
+
+cal.pack(padx=10, pady=10)
+
+
+def select_day_df(date, df=df):
+    """
+    Returns pd.Dataframe of selected day
+    """
+    selected_df = df[df['ts'].dt.date == pd.to_datetime(date).date()]
+    return selected_df
+
+
+def create_plot(df, predictions):
+    """
+    Creates a plot with comparison of prediction and real value
+    """
+    global current_canvas
+    # Clear the existing canvas if it exists
+    if current_canvas:
+        current_canvas.get_tk_widget().destroy()
+        current_canvas = None
+    
+    # the figure that will contain the plot 
+    fig = Figure(figsize = (10, 3), 
+                 dpi = 100) 
+  
+    # adding the subplot 
+    plot1 = fig.add_subplot(111) 
+  
+    # plotting the graph 
+    plot1.plot(df.ts, df['Friction'], label="Friction", color='b', linestyle=":")
+    plot1.plot(df.ts, predictions, label="Predicted", color='r')
+    plot1.legend()
+    plot1.grid(True)
+    # creating the Tkinter canvas 
+    # containing the Matplotlib figure 
+    current_canvas = FigureCanvasTkAgg(fig, 
+                               master = root)
+    current_canvas.draw() 
+  
+    # placing the canvas on the Tkinter window
+    current_canvas.get_tk_widget().grid(row=0, column=1, padx=10, pady=50)
+
+
+# Makes predictions for day dataset
+def on_confirm():
+    """
+    Main function that produces predictictions, slider, data lables
+    """
+    global day_df, predictions, slider
+    # Get date to parse a day
+    date_value = cal.get_date()
+    # Save day dataset into variable for making predictions
+    day_df = select_day_df(date_value, df)
+    # Reset index to make same indeces as in predictions list
+    # for proper comparison
+    day_df = day_df.reset_index(drop=True)
+    # Make predictions, turn them into dataframe and shift to 12 points
+    # to compare with original friction values
+    predictions = model.predict(day_df.drop("ts", axis=1))
+    predictions = pd.DataFrame(predictions, columns=["friction_fcst"])
+    predictions = predictions["friction_fcst"].shift(12)
+    # Print plot
+    create_plot(day_df, predictions)
+
+    # Clear existing slider if it exists
+    if slider:
+        slider.destroy()
+        slider = None
+
+    def on_slider_change(val):
+        """
+        On slider change prints out new data values and labels
+        """
+        index = int(float(val))
+        fcst_row = day_df[["ts", "airTemp", "humidity", "precipitation", 
+                      "dewpoint", "Road_Surface_Temperature", 
+                      "Water_Film_Surface_mm"]].iloc[index]
+        fcst_row_data = tk.Label(data_labels_frame, text=f"Original data: \n{fcst_row}")
+        fcst_row_data.grid(row=0, column=0)
+
+        predicted_friction_value = round(float(predictions[index]), 3)
+        predicted_friction_data = tk.Label(data_labels_frame, text=f"Predicted friction: \n{predicted_friction_value}")
+        predicted_friction_data.grid(row=0, column=1)
+
+        original_friction_value = round(float(day_df.Friction.iloc[index+12]), 3)
+        original_friction_data = tk.Label(data_labels_frame, text=f"Original Friction: \n{original_friction_value}")
+        original_friction_data.grid(row=0, column=2)
+        
+        accuracy = round((1 - abs(original_friction_value - predicted_friction_value) / abs(original_friction_value)) * 100, 2)
+        accuracy_data = tk.Label(data_labels_frame, text=f"Accuracy: \n{accuracy}%")
+        accuracy_data.grid(row=0, column=3)
+        # LIME explainer that tries to explane models' decision
+        exp = explainer.explain_instance(day_df.drop("ts", axis=1).values[index], model.predict)
+        # Extract feature importance
+        importances = exp.as_list()
+        importance_df = pd.DataFrame(importances, columns=['Feature', 'Importance'])
+        importance_df_data = tk.Label(data_labels_frame, text=f"Feature Importances using LIME: \n{importance_df}")
+        importance_df_data.grid(row=0, column=4)
+
+    # Initialize slider
+    slider = tk.Scale(
+        calendar_frame,
+        from_=day_df.index.min() + 12,
+        to=day_df.index.max()-12,
+        orient="horizontal",
+        label="Select data row",
+        command=on_slider_change
+    )
+    slider.pack(pady=10)
+ 
+# Add Button and Label
+tk.Button(calendar_frame, text = "Confirm",
+       command = on_confirm).pack()
+
+ 
+# Execute Tkinter
+root.mainloop()
+
